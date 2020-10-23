63ce64d3ef101941fa57cedcf547fb2500105048
pablosm02 <alum.psanchezm.2020@iesalixar.org>
2020-10-23 13:01:36 +0200

    acabe el 7 porfin

--- src/main/java/U2/unit2/U2/T7/t7_ex3.java
+++ src/main/java/U2/unit2/U2/T7/t7_ex3.java
@@ -1,8 +1,15 @@
 package U2.unit2.U2.T7;
-
+import com.sun.jdi.IntegerValue;
+import java.util.Random;
+import java.lang.*;
 public class t7_ex3 {
     public static void main(String[] args) {
-        
+        String s1= "abcdefghijklmnopqrstuvwxyz";
+        Double valorDado = Math.random()*26;
+        Integer valor = valorDado.intValue();
+        Character c1 = s1.charAt(valor); // 0-25
+        System.out.println("su letra es la " + c1);
+
 
 
     }
